5559730f06b0ea58452a4feb3f78bc3db0134aa2
HemingNM <neanderh@yahoo.com.br>
2018-07-03 16:36:51 -0300

    changed TODO

--- R/0. TODOs.R
+++ R/0. TODOs.R
@@ -1,7 +1,5 @@
 # TODOs
 
 # TODO - make a loop for each breeding season area
-# script file 3, line 476
-# also need to check how to deal with months of breeding
-# script file 3, line 259 & 277: m.b.LTM <- (m-1):m
+# use script file 3, line 258 to 436: ###### extrair registros de cada mês
 
